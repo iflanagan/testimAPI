82c12909fd648c6e4f3a29a327915b1a5f116217
Ian Flanagan <ianianf@yahoo.comi>
2023-01-18 17:18:37 -0600

    removed old comments.

--- src/com/IanFlanagan/MyConfiguration.java
+++ src/com/IanFlanagan/MyConfiguration.java
@@ -2,10 +2,7 @@ package com.IanFlanagan;
 
 public class MyConfiguration {
 
-    /*
-    old PAK-u91yh2jf1Ea0ky-KSe1UrDAcQL3cHDgI1dcl9ye2hvpLCyjq8AyQxvE1bVd3Dg0KiHVE+SctQVbihahir
-    new PAK-ibKrjfxAN0HH+q-KSNROmG1RB76GGtAbls+qSsBEKRHU3AJWeOl5gtyC+M2CZSAuCvtQSFBqs1/dYpqph
-     */
+
 
     // API OPTIONS GET POST PUT DELETE HEAD OPERATIONS PATCH
 
